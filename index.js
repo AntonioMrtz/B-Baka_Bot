5592eec5a6ca1aed0c5d5e0205edbbddafc7f95a
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2022-03-12 15:56:43 +0100

    help + bloodtrail + opgg

--- index.js
+++ index.js
@@ -23,15 +23,52 @@ client.on("ready", () => {
 
 })
 
+
 client.on("messageCreate",msg => {
 
+    //* FORMATO = https://euw.op.gg/champions/jhin/top/build
 
-    if(msg.content=="ping"){
+    //TODO checkear si existe campeon en pool campeones 
 
-        msg.reply("Pong");
+    if(msg.content=="!bloodtrail"){
+
+
+        msg.reply({files: [{ attachment: "bloodtrail.png" }] });
+        
     }
 
+    else if(msg.content.startsWith("!opgg")){
+
+
+        //TODO transform jgl -> jungla etc
+        let command = msg.content.split(" ");
+    
+        let url = "https://euw.op.gg/champions/";
+    
+        url+=command[1]+"/"+command[2]+"/"+"build";
+    
+       
+        console.log(url);
+    
+        msg.reply(url);
+            
+        
+
+
+
+    }
+    else if(msg.content=="!help"){
+
+        let help_commands = "```!opgg champion_name role```"; 
+        msg.reply(help_commands);
+        
+    }
+
+
+
 })
+
+
 
 
 client.login(process.env['TOKEN'])
