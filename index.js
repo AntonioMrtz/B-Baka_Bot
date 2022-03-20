4b86b1a25fa8ac108a3dbbf6e8faf502379d54b5
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2022-03-20 16:21:12 +0100

    OPGG MODULADO

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 
+const opgg = require('./node_modules/opgg/index.js')
+
 require('dotenv').config();
 
-//const mySecret = process.env['TOKEN'];
 
 const client = new Discord.Client({
     
@@ -40,105 +41,8 @@ client.on("messageCreate",msg => {
     
     else if(msg.content.startsWith("!opgg")){
         
-        
-        let flagTwoWordsChamp=0;
-        let voidChamp=0;
-        
-
-        let command = msg.content.split(" ");
-        
-        let url = "https://euw.op.gg/champions/";
-
-
        
-        let champion;
-        let role;
-
-        let champion_upperCaseFirst="";
-        
-        if(command.length==4){ // 2 word champs
-
-            flagTwoWordsChamp=1;
-            champion=command[1].toLocaleLowerCase()+command[2].toLocaleLowerCase();
-            role=command[3].toLocaleLowerCase();
-
-        }
-        else if(command[1].search("'")!=-1){ // has '
-           
-            voidChamp=1;
-            champion=command[1].toLocaleLowerCase();
-            role=command[2].toLocaleLowerCase();
-
-        }
-        else{ // single word
-            champion=command[1].toLocaleLowerCase();
-            role=command[2].toLocaleLowerCase();
-            
-        }
-        
-        champion=champion.trim();
-        role=role.trim();
-        
-        
-        
-        if (lol_champions.includes(champion) && lol_roles.includes(role)){  // check if the rol and champion exists
-            
-            if(role=="supp"){
-
-                role="support";
-            }
-            if(role=="jg" || role=="jgl"){
-
-                role="jungle";
-            }
-
-            if(champion.search("'")!=-1){
-                champion=champion.replace("'","");
-            }
-
-            url+=champion+"/"+role+"/"+"build";
-
-            
-            if(voidChamp==1){
-
-                champion_upperCaseFirst=champion[0].toUpperCase() + champion.slice(1);
-                champion_upperCaseFirst=champion_upperCaseFirst.replace("'","");
-                console.log(champion_upperCaseFirst)
-            }
-            else if(flagTwoWordsChamp==0){
-                
-                champion_upperCaseFirst= champion[0].toUpperCase() + champion.slice(1);
-               
-
-
-            }
-            else{
-
-                champion_upperCaseFirst=command[1][0].toUpperCase() + command[1].slice(1)+command[2][0].toUpperCase()+command[2].slice(1);
-                
-                
-            }
-            
-            console.log("Runas "+champion_upperCaseFirst+" en "+role);
-
-            let runesEmbeded = new Discord.MessageEmbed()
-            .setTitle("Runas "+champion_upperCaseFirst+" en "+role)
-            .setColor('FUCHSIA')
-            .setThumbnail("https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+champion_upperCaseFirst+"_0.jpg")
-            .addField("\u200B",url )
-            .addField("\u200B","\u200B" )
-            .setFooter({ text: 'Op.GG', iconURL: 'https://raw.githubusercontent.com/AntonioMrtz/B-Baka_Bot/main/img/opgg_logo.png' })
-            .setTimestamp()
-            .setURL(url)
-    
-    
-            msg.reply({ embeds : [runesEmbeded] });
-        }
-        else{
-
-            msg.reply("```Champion or Role not found```")
-        }
-        
+        opgg.queryOpgg(msg,lol_champions,lol_roles);
     }
     else if(msg.content=="!help"){
         
@@ -189,7 +93,6 @@ client.on("messageCreate",msg => {
     else if(msg.content=="!p"){ //!PRUEBAS
 
         
-
         
 
     }
