80729fbbafb04e9864c1160d50ae9f9eb2c625bd
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2022-04-18 20:00:50 +0200

    Urban dictionary functional

--- index.js
+++ index.js
@@ -11,21 +11,99 @@ const axios = require('axios');
 require('dotenv').config();
 
 
-async function fetchResponse(msg){
+async function fetchResponse(msg,word){
 
-    let url = 'https://api.urbandictionary.com/v0/define?term=stonks';
-    //let url="https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=USD"
+
+    /*
+    {   INPUT FROM Urban dictionary API
+    "list": [
+      "definition": "A [set] of man [fruits], [love] spudsbollocksgooliesknackers",
+      "permalink": "http://stonks.urbanup.com/1709089",
+      "thumbs_up": 14,
+      "sound_urls": [],
+      "author": "Rudis",
+      "word": "stonks",
+      "defid": 1709089,
+      "current_vote": "",
+      "written_on": "2006-04-20T07:16:23Z",
+      "example": "he had a [set] of stonks like a fighters [speed ball]",
+      "thumbs_down": 29
+      ]
+    }
+    */
+
+    //word = "take it easy";  // ! QUITAR DSP 
+    let url = 'https://api.urbandictionary.com/v0/define?term='+word;
+    
 
     axios.get(url)
     .then((res) => {
 
-        console.log(res.data.list)
-        msg.reply(JSON.stringify(res.data.list[0]))
+        let max_upvotes = -1;
+        let index_max_upvotes=-1;
+
+        for(let i=0;i<res.data.list.length;i++){
+
+            
+            if(max_upvotes==-1 || max_upvotes<res.data.list[i].thumbs_up){
+                max_upvotes=res.data.list[i].thumbs_up;
+                index_max_upvotes=i;
+            }
+
+        }
+
+        if(max_upvotes!=-1){
+
+            let definition = res.data.list[index_max_upvotes].definition;
+            definition=definition.replaceAll("[","");
+            definition=definition.replaceAll("]","");
+
+            let example = res.data.list[index_max_upvotes].example;
+            example=example.replaceAll("[","");
+            example=example.replaceAll("]","");
+
+            let url=res.data.list[index_max_upvotes].permalink;
+
+
+
+            let urbanDictionaryEmbeded = new Discord.MessageEmbed()
+                .setTitle("Definition of "+word)
+                .setThumbnail("https://raw.githubusercontent.com/AntonioMrtz/B-Baka_Bot/main/img/ud_logo.png")
+                .setColor('DARK_BLUE')
+                .addField("\u200B",definition)
+                .addField("\u200B","Example/s: ")
+                .addField("\u200B",example)
+                .addField("\u200B","\u200B")
+                .setFooter({ text: 'Urban Dictionary Api', iconURL: "https://raw.githubusercontent.com/AntonioMrtz/B-Baka_Bot/main/img/ud_logo.png" })
+                .setTimestamp()
+                .setURL(url)
+    
+        
+        
+            msg.reply({ embeds : [urbanDictionaryEmbeded] });
+    
+            //msg.reply(JSON.stringify(res.data.list[index_max_upvotes]))
+            
+        }
+        else{
+
+            let urbanDictionaryEmbeded = new Discord.MessageEmbed()
+            .setTitle("*Definition not found*")
+            .setThumbnail("https://raw.githubusercontent.com/AntonioMrtz/B-Baka_Bot/main/img/ud_logo.png")
+            .setColor('DARK_RED')
+            .setFooter({ text: 'Urban Dictionary Api', iconURL: "https://raw.githubusercontent.com/AntonioMrtz/B-Baka_Bot/main/img/ud_logo.png" })
+            .setTimestamp()
+
+    
+    
+            msg.reply({ embeds : [urbanDictionaryEmbeded] });
+
+
+        }
 
     })
   
     
-    //TODO ordenar respuestas por valoracion
     
 }
 
@@ -134,7 +212,10 @@ client.on("messageCreate",msg => {
             
         */
 
-        fetchResponse(msg);
+        let command = msg.content.split(" ");
+        command = command.slice(1);
+        console.log(command)
+        //fetchResponse(msg,word);
 
 
 
@@ -145,6 +226,15 @@ client.on("messageCreate",msg => {
         console.log('\nREQUEST 3', await(await fetch(url)).json()) 
         )();*/
 
+
+    }
+    else if(msg.content.startsWith("!definition")){
+
+        let command = msg.content.split(" ");
+        command = command.slice(1);
+        command = command.join().trim().replaceAll(","," ");
+
+        fetchResponse(msg,command)
 
     }
     else if(msg.content=="!rampas"){
